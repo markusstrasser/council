28165aea7e08e43715f3d398d9f13aa3bbefa4c0
Markus Strasser <strasser.ms@gmail.com>
2022-11-01 09:52:45 -0700

    streamlit cleanup

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import torch
 import numpy as np
 import pandas as pd
 import whisper
-
+import json
 
 from haystack.nodes import EmbeddingRetriever, FARMReader
 from haystack.document_stores import ElasticsearchDocumentStore, InMemoryDocumentStore 
@@ -21,21 +21,27 @@ from haystack.pipelines import ExtractiveQAPipeline
 @st.cache(allow_output_mutation=True)
 def load_model():
     #write a function that adds two numbers
-
-    df = pd.read_json("example_whisper_transcript")
+    transcripts = "example_whisper_transcript.json"
+    with open(transcripts) as f:
+        data = json.loads(f.read())
+    #print(data['data'][0])
+    df = pd.DataFrame.from_records(data["segments"])
     print(df)
     ds = df.to_dict("records")
 
+
+    #! THIS CAUSES SEGMENTATION FAULTS? 
     document_store = InMemoryDocumentStore(embedding_dim=384, similarity="cosine")
 
     # document_store = ElasticsearchDocumentStore(similarity="cosine",
     #                                             embedding_dim=384)
-
     dicts = [{"content": d["text"], "meta": {i:d[i] for i in d if i!='text'}} for d in ds]
 
-    document_store.write_documents(dicts)
-    retriever = EmbeddingRetriever(document_store=document_store, embedding_model='sentence-transformers/all-MiniLM-L6-v2', use_gpu=True, top_k=10)
+    document_store.write_documents(dicts[:10])
+    print("doc store")
+    retriever = EmbeddingRetriever(document_store=document_store, embedding_model='sentence-transformers/all-mpnet-base-v2', use_gpu=True, top_k=10)
 
+    print("retriever")
     document_store.update_embeddings(retriever)
     reader = FARMReader(model_name_or_path='deepset/roberta-base-squad2', use_gpu=True)
     # result = reader.predict(
@@ -43,6 +49,7 @@ def load_model():
     #     documents=documents,
     #     top_k=10
     # )
+    print("before pipe")
     pipe = ExtractiveQAPipeline(reader, retriever)
     return pipe
 pipe = load_model()
